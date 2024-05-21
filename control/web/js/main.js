91b69326e4a5eaa48e4ed19e665d20c707ee2323
Angel-Gaston <angelgastona@gmail.com>
2024-05-21 08:41:02 -0600

    Add files via upload

---
+++ control/web/js/main.js
@@ -0,0 +1,45 @@
+//alert("Hola mundo");
+let adelante =document.getElementById("adelante");
+let atras =document.getElementById("atras");
+let izquierda =document.getElementById("izquierda");
+let derecha =document.getElementById("derecha");
+let detener =document.getElementById("detener");
+
+let mensaje=document.getElementById("mensaje");
+
+function callApi(estatus) {
+  // Hacer una petición para un usuario con ID especifico
+  axios
+    .get(
+      "http://localhost/iot-car-control/back-end/apis/setRegistro.php?valorEstatus=" +
+        estatus
+    )
+    .then(function (response) {
+      // manejar respuesta exitosa
+      console.log(response);
+      mensaje.innerHTML="Respuesta: <strong>" + response.data + "</strong>";
+    })
+    .catch(function (error) {
+      // manejar error
+      console.log(error);
+    })
+    .finally(function () {
+      // siempre sera executado
+    });
+}
+
+adelante.addEventListener("click", function () {
+    callApi('w');
+});
+atras.addEventListener("click", function () {
+  callApi('s');
+});
+izquierda.addEventListener("click", function () {
+  callApi('a');
+});
+derecha.addEventListener("click", function () {
+  callApi('d');
+});
+detener.addEventListener("click", function () {
+  callApi('x');
+});
